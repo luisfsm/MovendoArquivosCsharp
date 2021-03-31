d77681dc1826c0447ce4b3abe30d0b4cf0f8b8d5
luis felipe <luis.s.mendes20@gmail.com.br>
2021-03-31 11:09:36 -0300

    criando o  primeiro escopo

---
+++ MoverArquviosCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoverArquviosCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
